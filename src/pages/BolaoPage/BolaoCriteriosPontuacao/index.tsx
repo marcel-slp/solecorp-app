f2bd1cb3fc29790f38898c18022c651fb9dbd261
Marcel Toledo <marcel.slp@gmail.com>
2026-04-17 20:26:29 +0100

    regra para bloquear user free de editar criterios de pontuacao

--- src/pages/BolaoPage/BolaoCriteriosPontuacao/index.tsx
+++ src/pages/BolaoPage/BolaoCriteriosPontuacao/index.tsx
@@ -2,11 +2,12 @@ import * as styles from "./styles.css";
 import TabelasCriteriosPontuacao from "../../../components/TabelasCriteriosPontuacao";
 import { BolaoRoles } from "../../../models/BolaoCopaDefault";
 import { bolaoStore } from "../../../stores/bolaoStore";
+import { retornaUserPerfil } from "../../../utils/Utils";
 
 export default function BolaoCriteriosPontuacao() {
   const { participanteBolaoLogado } = bolaoStore();
 
-  const pontosEditaveisInterno = participanteBolaoLogado?.roleBolao === BolaoRoles.CRIADOR;
+  const pontosEditaveisInterno = participanteBolaoLogado?.roleBolao === BolaoRoles.CRIADOR && retornaUserPerfil() !== 'a2b1c';
   
   return (
     <div className={styles.tableCriterioContainer}>
